dcc1444b0ab84e0120e66342fbe866609dc977f2
Pranav Vashi <neobuddy89@gmail.com>
2020-04-05 16:57:55 +0530

    sm6150-common: Remove blob fixup for android.hidl.base
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -58,10 +58,6 @@ function blob_fixup() {
         vendor/bin/mlipayd@1.1 | vendor/lib64/libmlipay.so | vendor/lib64/libmlipay@1.1.so )
                 patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
         ;;
-	lib64/libwfdnative.so | lib64/libfm-hci.so | lib/libfm-hci.so | vendor/bin/hw/vendor.display.color@1.0-service | \
-		vendor/lib64/ftm_fm_lib.so | vendor/lib/ftm_fm_lib.so | vendor/lib64/libgoodixhwfingerprint.so )
-		patchelf --remove-needed "android.hidl.base@1.0.so" "${2}"
-        ;;
         esac
 }
 
